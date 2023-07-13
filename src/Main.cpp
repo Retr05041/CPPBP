b2388ebe125b858f736984bc747a0ae652f0031f
Retr05041 <parker@cranfield.ca>
2023-07-12 21:49:24 -0700

    Add Problem to runner

---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+// Runner
+
+#include <iostream>
+#include "problem/Problem.hpp"
+
+int main(int argc, char const *argv[]) {
+    Problem problem("../test_instances/d1_50_500.txt");
+    return 0;
+}
